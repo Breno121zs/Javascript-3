eb571e893c8be044184bb2afcba3e7ab51535f9f
Breno1221 <123486496+Breno121zs@users.noreply.github.com>
2024-04-09 10:30:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const firstName = 'Sara';
+
+// Numbers
+const age = 30;
+const temp = 98.9;
+
+
+const hasKids = true;
+
+
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 90007n;
+
+//reference types
+
+const Numbers = [1, 2, 3, 4];
+
+const person = {
+    name: 'Brad',
+}
+
+function sayHello () {
+    console.log ('Hello');
+}
+
+const output = sayHello;
+
+console.log (output, typeof output)
